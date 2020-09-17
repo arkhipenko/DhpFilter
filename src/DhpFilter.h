262c621d0e7bdd9e8efb5e7029bd04aca9c3f12c
Anatoli Arkhipenko <arkhipenko@hotmail.com>
2020-09-16 21:01:58 -0400

    various updates

--- src/DhpFilter.h
+++ src/DhpFilter.h
@@ -12,34 +12,35 @@
 
 class dhpFilter {
 
-	public: 
-		dhpFilter(long aInitValue=0);
-	
-		void	initialize(long aInitValue);
-		long value(long aSample);
+    public: 
+        dhpFilter(long aInitValue=0);
+    
+        void    initialize(long aInitValue=0);
+        long    value(long aSample);
+        inline long currentValue() { return (iShiftedFilter+128)>>8; };
 
-	private:
-		long	iLastSample;
-		long	iShiftedFilter;
+    private:
+        long    iLastSample;
+        long    iShiftedFilter;
 };
 
 dhpFilter::dhpFilter(long aInitValue) {
-	initialize(aInitValue);
+    initialize(aInitValue);
 }
 
 void dhpFilter::initialize(long aInitValue) {
-	iLastSample = aInitValue;
-	iShiftedFilter = aInitValue << 8;
+    iLastSample = aInitValue;
+    iShiftedFilter = aInitValue << 8;
 }
 
 
 long dhpFilter::value(long aSample) {
 
-	long shiftedFCL = iShiftedFilter + ((aSample-iLastSample)<<8);
+    long shiftedFCL = iShiftedFilter + ((aSample-iLastSample)<<8);
 
-	iLastSample = aSample;
-	iShiftedFilter = shiftedFCL - 5*(shiftedFCL>>7);
-	return (iShiftedFilter+128)>>8;
+    iLastSample = aSample;
+    iShiftedFilter = shiftedFCL - 5*(shiftedFCL>>7);
+    return (iShiftedFilter+128)>>8;
 }
 
 #endif  // _DHPFILTER_H
